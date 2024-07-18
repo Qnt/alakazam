392eef52e2a80a2ca29ed5f28cd59391abfbbd8e
Ainur <lihodeikvint@gmail.com>
2024-07-18 15:12:40 +0500

    fix: add protected route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider afterSignOutUrl={"/"}>
       <html
         lang="en"
         className={`${GeistSans.variable} ${lilitaOne.variable}`}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/home(.*)", "/collections(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/home(.*)",
+  "/collections(.*)",
+  "/cards(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
   if (isProtectedRoute(req)) {
     auth().protect();
   }
-  if (auth().userId && req.nextUrl.pathname === "/") {
+  if (userId && req.nextUrl.pathname === "/") {
     const url = new URL("/home", req.nextUrl.origin);
     return NextResponse.redirect(url);
   }
