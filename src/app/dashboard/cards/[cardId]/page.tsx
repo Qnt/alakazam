4a286d0ddc6352df8015871a5c58081a4a95aaeb
Ainur <lihodeikvint@gmail.com>
2024-07-18 18:53:10 +0500

    add: stats on card page

--- src/app/dashboard/cards/[cardId]/page.tsx
+++ src/app/dashboard/cards/[cardId]/page.tsx
@@ -15,9 +15,9 @@ export default async function CardPage({
   if (!card) {
     notFound();
   }
-
+  console.log(card);
   return (
-    <div className="flex flex-col justify-between gap-2">
+    <div className="flex flex-col justify-between gap-4">
       <div className="flex justify-between gap-2">
         <Link
           href={`/dashboard/collections/${card.collectionId}`}
@@ -29,27 +29,39 @@ export default async function CardPage({
         <div className="flex gap-2">
           <Link
             href={`/dashboard/cards/${params.cardId}/edit`}
-            className="btn btn-primary btn-sm items-center"
+            className="btn btn-primary btn-sm flex-nowrap items-center"
           >
             <SquarePen size={16} />
-            <span>Edit</span>
+            <span className="hidden md:block">Edit</span>
           </Link>
           <form action={deleteCard.bind(null, card)}>
-            <ButtonSubmit className="btn btn-error btn-sm items-center">
+            <ButtonSubmit className="btn btn-error btn-sm flex-nowrap items-center">
               <Trash2 size={16} />
-              <span>Delete</span>
+              <span className="hidden md:block">Delete</span>
             </ButtonSubmit>
           </form>
         </div>
       </div>
-      <div className="flex max-w-xl flex-col gap-4 self-center">
-        <div className="flex flex-col">
-          <p>Question:</p>
-          <h2 className="text-lg font-semibold">{card.question}</h2>
-        </div>
-        <div className="flex flex-col">
-          <p>Answer:</p>
-          <p>{card.answer}</p>
+      <div className="card mx-auto max-w-xl bg-base-200 shadow-md">
+        <div className="card-body">
+          <h2 className="card-title">{card.question}</h2>
+          <p className="pb-4">{card.answer}</p>
+          <div className="grid grid-cols-2 gap-2">
+            <p>Box:</p>
+            <div className="badge badge-outline lowercase">{card.box}</div>
+            <p>Created:</p>
+            <div className="">{card.createdAt.toLocaleString()}</div>
+            <p>Updated:</p>
+            <div className="">{card.updatedAt.toLocaleString()}</div>
+            <p>Last answered:</p>
+            <div className="">
+              {card.lastAnswered ? card.lastAnswered.toLocaleString() : "never"}
+            </div>
+            <p>Correct answers:</p>
+            <div className="">{card.answeredCorrectly}</div>
+            <p>Incorrect answers:</p>
+            <div className="">{card.answeredWrongly}</div>
+          </div>
         </div>
       </div>
     </div>
