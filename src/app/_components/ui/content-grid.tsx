ed9cea5d4c43c64b557fd0fe3257c36cb95a73e1
Ainur <lihodeikvint@gmail.com>
2024-07-22 15:44:36 +0500

    fix: delete mx-auto

--- src/app/_components/ui/content-grid.tsx
+++ src/app/_components/ui/content-grid.tsx
@@ -4,7 +4,7 @@ export default function ContentGrid({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto flex flex-col gap-6 sm:grid sm:grid-cols-[repeat(auto-fill,minmax(20rem,1fr))]">
+    <div className="flex flex-col gap-6 sm:grid sm:grid-cols-[repeat(auto-fill,minmax(20rem,1fr))]">
       {children}
     </div>
   );
