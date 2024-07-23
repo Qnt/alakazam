69115c85fadef0eaf6430dc4ec60357c2fd3bc55
Ainur <lihodeikvint@gmail.com>
2024-07-23 22:51:18 +0500

    change: redirect after sign up and sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
   if (isProtectedRoute(req)) {
     auth().protect();
   }
-  // if (userId && req.nextUrl.pathname === "/") {
-  //   const url = new URL("/dashboard", req.nextUrl.origin);
-  //   return NextResponse.redirect(url);
-  // }
 });
 
 export const config = {
